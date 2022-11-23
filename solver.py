af0f5d173892a9f78ba4ff8ad225e2a2dc263c6c
Theodor Negrescu <theodor.negrescu@gmail.com>
2022-11-24 01:21:06 +0200

    Improve efficiency

--- solver.py
+++ solver.py
@@ -5,24 +5,24 @@ def calculate_entropy(guess, answer_space):
         def letter_info(pos):
             if guess[pos] == answer[pos]:
                 return constants.FULL_MATCH
-            elif guess[pos] in answer[pos]:
+            elif guess[pos] in answer:
                 return constants.PARTIAL_MATCH
             else:
                 return constants.NO_MATCH
-        return [letter_info(pos) for pos in range(len(guess))]
+        return [(letter_info(pos), pos) for pos in range(len(guess))]
     entropy = 0
-    proc = 0
     for answer in answer_space:
-        matching = 0
-        match = get_match_info(guess, answer)
-        for word in answer_space:
-            if get_match_info(word, answer) == match:
-                matching += 1
-        information = math.log2(len(answer_space) / matching)
+        match = sorted(get_match_info(guess, answer), reverse=True)
+        match_space = answer_space
+        for match_code, match_pos in match:
+            if match_code == constants.FULL_MATCH:
+                match_space = [x for x in match_space if x[match_pos] == answer[match_pos]]
+            elif match_code == constants.PARTIAL_MATCH:
+                match_space = [x for x in match_space if x[match_pos] != answer[match_pos] and x[match_pos] in answer]
+            else:
+                match_space = [x for x in match_space if x[match_pos] not in answer]
+        information = math.log2(len(answer_space) / len(match_space))
         entropy += information
-        if not (proc % 100):
-            print(proc / len(answer_space) * 100, '%')
-        proc += 1
     entropy /= len(answer_space)
     return entropy
 
@@ -34,7 +34,10 @@ def main():
     except FileNotFoundError:
         print(f"{constants.DICT} not found")
         sys.exit(2)
-    entropy_values = [calculate_entropy(word, dict) for word in dict]
+    entropy_values = [0] * len(dict)
+    for i in range(len(dict)):
+        entropy_values[i] = calculate_entropy(dict[i], dict)
+        print((i + 1) / len(dict) * 100, '%')
     print(entropy_values)
 
 if __name__ == "__main__":
