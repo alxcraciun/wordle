366f74f087ff6ae4ac1da4a150f4aba09e116f61
Alex Craciun <56975249+alxcraciun@users.noreply.github.com>
2022-11-25 00:03:38 +0200

    interfata gui

--- gui.py
+++ gui.py
@@ -1,2 +1,21 @@
 from tkinter import *
 root = Tk()
+
+# Label1 = Label(root, text = 'Wordle Project')
+# Label2 = Label(root, text = 'Please submit your word:').grid(row = 0, column = 10)
+
+def myClick():
+    if myButton['state'] == 'normal':
+        myButton['state'] = 'disabled'
+    user_name = e.get()
+    myLabel = Label(root, text = f'{user_name} a introdus un cuvant')
+    myLabel.grid(row = 2, column= 0)
+
+myButton = Button(root, text = "Click me!", state = 'normal', padx = 15, pady = 5, command = myClick)
+
+e = Entry(root, borderwidth=20)
+e.grid(row = 0, column = 0)
+
+myButton.grid(row = 1, column=0)
+
+root.mainloop()
