4c9611d6f529705386b7fbefb6e9b1c8fba3fd5b
Theodor Negrescu <theodor.negrescu@gmail.com>
2022-11-27 14:08:32 +0200

    Use threads for input in wordle.py
    Preparing to merge IPC

--- wordle.py
+++ wordle.py
@@ -1,42 +1,79 @@
-print()
-
-import random
-
-def new_guess():
-    guess = input('')
-    guess = guess.replace(' ', '')
-    guess = list(guess)
-    return guess
+import random, sys, queue, argparse, dataclasses, threading
 
 database = []
 
 try:
-    fisier = open('cuvinte_wordle.txt')
+    database = open('cuvinte_wordle.txt').read().split()
 except:
     print("Fisierul 'cuvinte_wordle.txt' nu exista")
+    sys.exit(1)
 
-database = [cuvant for cuvant in fisier.read().split()]
+@dataclasses.dataclass(frozen=True)
+class TermInput:
+    input: str
 
-response = list(random.choice(database))
+@dataclasses.dataclass(frozen=True)
+class TermOutput:
+    output: str
+    end: bool
 
-for letter in response:
-    print(letter, end = ' ')
-print('\n------------')
+input_queue = queue.Queue() # TermInput - input from console, must be responded to through console_out_queue.
+                            # Exception - an input thread has exited
+console_out_queue : queue.Queue[TermOutput] = queue.Queue()
 
-guess = new_guess()
+def user_input_thread():
+    try:
+        while True:
+            word = input("GUESS = ")
+            input_queue.put(TermInput(word))
+            out = console_out_queue.get()
+            print(out.output)
+            if out.end:
+                input_queue.put(SystemExit())
+                break
+    except (KeyboardInterrupt, EOFError) as e:
+        input_queue.put(e)
 
-while guess != response:
-    for poz in range(5):
-        if guess[poz] == response[poz]:
-            print('✓', end = ' ')
-        elif guess[poz] in response:
-            print('✧', end = ' ')
-        else:
-            print('✗', end = ' ')
+
+def validate_word(word : str):
+    if len(word) == 5 and word.isupper():
+        return word
+    raise argparse.ArgumentTypeError("invalid word")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--secret-word', default=None, type=validate_word)
+    args = parser.parse_args()
+    if args.secret_word == None:
+        secret_word = random.choice(database)
     else:
-        print('\n')
-    guess = new_guess()
-else:
-    print('✓ ✓ ✓ ✓ ✓')
+        secret_word = args.secret_word
+    console_reader = threading.Thread(group=None, target=user_input_thread)
+    console_reader.daemon = True
+    console_reader.start()
+    #TODO: IPC thread
+    threads = [console_reader]
+    while True:
+        msg = input_queue.get()
+        if isinstance(msg, BaseException):
+            if True not in [t.is_alive() for t in threads]:
+                break
+            else:
+                continue
+        elif isinstance(msg, TermInput):
+            guess = msg.input
+        else:
+            raise TypeError()
+        out = str()
+        for poz in range(5):
+            if secret_word[poz] == guess[poz]:
+                out += '✓'
+            elif guess[poz] in secret_word:
+                out += '✧'
+            else:
+                out += '✗'
+        if isinstance(msg, TermInput):
+            console_out_queue.put(TermOutput(guess + "\n" + out + "\n", out == "✓✓✓✓✓"))
 
-print()
+if __name__ == "__main__":
+    main()
