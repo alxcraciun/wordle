1d1fb6c8650d19ac3baa69eca9116fbe1e1e8fad
Theodor Negrescu <theodor.negrescu@gmail.com>
2022-11-27 16:50:43 +0200

    Finish simultaneous IPC and terminal input

--- ipc.py
+++ ipc.py
@@ -1,9 +1,11 @@
-import sys, constants
+import sys, os, constants, traceback
+
+def err():
+    print("IPC ERROR")
+    write("ERR", 100, False)
+    os._exit(constants.ERROR_IPC)
 
 if constants.DEBUG_IPC:
-    def err():
-        print("IPC ERROR")
-        sys.exit(constants.ERROR_IPC)
 
     def read():
         i = input("READ:")
@@ -15,9 +17,6 @@ if constants.DEBUG_IPC:
         print(f"WRITE:{msg}")
 else:
     import http.server as server, http.client as client
-    def err():
-        print("IPC ERROR")
-        sys.exit(constants.ERROR_IPC)
     def read():
         data = [None]
         class RequestHandler(server.BaseHTTPRequestHandler):
@@ -32,15 +31,17 @@ else:
                 return
         svr = server.HTTPServer(('localhost', constants.PORT), RequestHandler)
         svr.handle_request()
+        svr.server_close()
         return data[0].decode("utf-8")
-    def write(msg):
+    def write(msg, timeout=1, retry=True):
         while True:
             try:
-                c = client.HTTPConnection(f"localhost:{constants.PORT}")
+                c = client.HTTPConnection(f"localhost:{constants.PORT}", timeout=timeout)
                 c.request("POST", "/", msg.encode("utf-8"))
                 r = c.getresponse()
                 c.close()
                 if r.getcode() == 200:
                     break
-            except BaseException as e:
-                continue
+            except Exception as e:
+                if not retry:
+                    break
--- wordle.py
+++ wordle.py
@@ -1,4 +1,4 @@
-import random, sys, queue, argparse, dataclasses, threading
+import random, sys, queue, argparse, dataclasses, threading, constants, ipc
 
 database = []
 
@@ -6,7 +6,7 @@ try:
     database = open('cuvinte_wordle.txt').read().split()
 except:
     print("Fisierul 'cuvinte_wordle.txt' nu exista")
-    sys.exit(1)
+    sys.exit(constants.ERROR_FILE_NOT_FOUND)
 
 @dataclasses.dataclass(frozen=True)
 class TermInput:
@@ -17,28 +17,60 @@ class TermOutput:
     output: str
     end: bool
 
-input_queue = queue.Queue() # TermInput - input from console, must be responded to through console_out_queue.
-                            # Exception - an input thread has exited
-console_out_queue : queue.Queue[TermOutput] = queue.Queue()
+@dataclasses.dataclass(frozen=True)
+class IPCInput:
+    input: str
 
-def user_input_thread():
+@dataclasses.dataclass(frozen=True)
+class IPCOutput:
+    output: str
+    end: bool
+
+input_queue = queue.Queue()
+ipc_out_queue : queue.Queue[IPCOutput] = queue.Queue()
+
+def ipc_input_thread():
     try:
         while True:
-            word = input("GUESS = ")
-            input_queue.put(TermInput(word))
-            out = console_out_queue.get()
-            print(out.output)
+            word = ipc.read()
+            input_queue.put(IPCInput(word))
+            out = ipc_out_queue.get()
+            ipc.write(out.output)
             if out.end:
-                input_queue.put(SystemExit())
                 break
+    except Exception as e:
+        ipc.err()
+
+def user_input_thread():
+    if constants.DEBUG_IPC:
+        while True:
+            pass
+    try:
+        while True:
+            word = input()
+            input_queue.put(TermInput(word))
     except (KeyboardInterrupt, EOFError) as e:
         input_queue.put(e)
 
+def get_match_info(guess, answer):
+    assert(len(guess) == len(answer))
+    def get_letter_info(pos):
+        if guess[pos] == answer[pos]:
+            return constants.FULL_MATCH
+        elif guess[pos] in answer:
+            return constants.PARTIAL_MATCH
+        else:
+            return constants.NO_MATCH
+    return [get_letter_info(i) for i in range(len(answer))]
 
 def validate_word(word : str):
     if len(word) == 5 and word.isupper():
         return word
     raise argparse.ArgumentTypeError("invalid word")
+
+def match_to_unicode(match : list[int]):
+    conv = {2:'\u2713',1:'\u2727',0:'\u2717'}
+    return "".join([conv[code] for code in match])
 
 def main():
     parser = argparse.ArgumentParser()
@@ -51,29 +83,39 @@ def main():
     console_reader = threading.Thread(group=None, target=user_input_thread)
     console_reader.daemon = True
     console_reader.start()
-    #TODO: IPC thread
-    threads = [console_reader]
-    while True:
+    ipc_reader = threading.Thread(group=None, target=ipc_input_thread)
+    ipc_reader.daemon = True
+    ipc_reader.start()
+    guess_history = []
+    while [constants.FULL_MATCH] * 5 not in guess_history:
         msg = input_queue.get()
         if isinstance(msg, BaseException):
-            if True not in [t.is_alive() for t in threads]:
-                break
-            else:
-                continue
+            raise msg
         elif isinstance(msg, TermInput):
+            guess = msg.input
+        elif isinstance(msg, IPCInput):
+            if msg.input == "LIST":
+                it = iter(guess_history)
+                ipc_out_queue.put(IPCOutput(" ".join(f"{word}={''.join([str(x) for x in match])}" for word, match in zip(it, it)), False))
+                continue
+            elif len(msg.input) != 5 or not msg.input.isupper():
+                ipc.err()
             guess = msg.input
         else:
             raise TypeError()
-        out = str()
-        for poz in range(5):
-            if secret_word[poz] == guess[poz]:
-                out += '✓'
-            elif guess[poz] in secret_word:
-                out += '✧'
-            else:
-                out += '✗'
-        if isinstance(msg, TermInput):
-            console_out_queue.put(TermOutput(guess + "\n" + out + "\n", out == "✓✓✓✓✓"))
+        if guess not in database:
+            print("Invalid guess.")
+            continue
+        match = get_match_info(guess, secret_word)
+        won = match == [constants.FULL_MATCH] * 5
+        print(guess + "\n" + match_to_unicode(match) + "\n")
+        if isinstance(msg, IPCInput):
+            ipc_out_queue.put(IPCOutput("OK", False) if not won else IPCOutput("CASTIGAT", True))
+            if won:
+                while ipc_reader.is_alive():
+                    pass # let IPC finish before exiting
+        guess_history.append(guess)
+        guess_history.append(match)
 
 if __name__ == "__main__":
     main()
